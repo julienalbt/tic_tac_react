6ec636365f485c5b336d3679fba104129325ed44
julien <alombertjulien@gmail.com>
2021-05-13 19:12:22 +0200

    add end of game

--- src/index.js
+++ src/index.js
@@ -30,6 +30,10 @@ import './index.css';
 
     handleClick(i) {
       const squares = this.state.squares.slice();
+      
+      if (calculateWinner(squares) || squares[i]) {
+        return;
+      }
       squares[i] = this.state.xIsNext ? 'X' : 'O';
       this.setState({
         squares: squares,
@@ -38,8 +42,25 @@ import './index.css';
     }
   
     render() {
-      const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-  
+      const squares = this.state.squares.slice();
+      const winner = calculateWinner(this.state.squares);
+      let status;
+      let fullArray = [];
+      if (winner) {
+        status = 'Winner: ' + winner;
+      } else {
+        status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      }
+
+      for (let i = 0; i < this.state.squares.length; i++) {
+        if (squares[i] != null) {
+          fullArray.push(squares[i]);
+        }
+        if (fullArray.length === 9 && !winner) {
+          status = 'No winner';
+        }
+      }
+
       return (
         <div>
           <div className="status">{status}</div>
@@ -86,3 +107,22 @@ import './index.css';
     document.getElementById('root')
   );
   
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
